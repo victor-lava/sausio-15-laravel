685dbc083028cedbf1e048922189e38404d87ad0
vagrant <vagrant@homestead>
2019-02-23 19:44:56 +0000

    added broadcasting with puser

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,7 +5,7 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
- // require('./bootstrap');
+ require('./bootstrap');
  window.table = document.querySelector('.table');
 
 const API = require('./api'),
